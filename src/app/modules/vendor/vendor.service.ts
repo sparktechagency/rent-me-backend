21e79d4f4fda054cb6abc3e9a636c30633e777a8
Asaduzzama-n <asad_uzzaman@outlook.com>
2024-11-17 14:08:27 +0600

    done refactoring analytics api

--- src/app/modules/vendor/vendor.service.ts
+++ src/app/modules/vendor/vendor.service.ts
@@ -10,12 +10,11 @@ import {
   buildDateTimeFilter,
   buildRangeFilter,
   findVendorsByBudget,
-  getIntervals,
+  getDateRangeAndIntervals,
+  mapDataToIntervals,
 } from './vendor.utils';
 import { Service } from '../service/service.model';
 import { Order } from '../order/order.model';
-import { getEvenlyDistributedData } from '../../../helpers/statDataHelper';
-import { getStartDate } from '../../../util/date';
 
 const updateVendorProfile = async (
   user: JwtPayload,
@@ -227,22 +226,8 @@ const getVendorRevenue = async (
   range: string = '1'
 ): Promise<{ key: string; value: number }[]> => {
   try {
-    const months = parseInt(range, 10) || 1;
-    const endDate = new Date();
-    const startDate = new Date();
-    startDate.setMonth(endDate.getMonth() - months);
-
-    const intervalDays = (months * 30) / 10;
-    const intervalMilliseconds = intervalDays * 24 * 60 * 60 * 1000;
-
-    const totalIntervals = Math.floor(
-      (endDate.getTime() - startDate.getTime()) / intervalMilliseconds
-    );
-
-    const intervals = Array.from({ length: totalIntervals }, (_, i) => ({
-      key: `${i * intervalDays + 1}-${(i + 1) * intervalDays}`,
-      value: 0,
-    }));
+    const { startDate, endDate, intervals, intervalMilliseconds } =
+      getDateRangeAndIntervals(range);
 
     const revenueData = await Order.aggregate([
       {
@@ -273,14 +258,7 @@ const getVendorRevenue = async (
       },
     ]);
 
-    revenueData.forEach(({ _id, totalRevenue }) => {
-      if (_id === intervals.length)
-        intervals[intervals.length - 1].value += totalRevenue;
-
-      if (_id < intervals.length) {
-        intervals[_id].value = totalRevenue;
-      }
-    });
+    mapDataToIntervals(intervals, revenueData, 'totalRevenue');
 
     return intervals;
   } catch (error) {
@@ -293,24 +271,8 @@ const getVendorOrders = async (
   range: string = '1' // Default to 1 month
 ): Promise<{ key: string; value: number }[]> => {
   try {
-    const months = parseInt(range, 10) || 1;
-
-    const endDate = new Date();
-    const startDate = new Date();
-    startDate.setMonth(endDate.getMonth() - months);
-
-    const intervalDays = (months * 30) / 10; // 3, 6, or 9 days based on months
-    const intervalMilliseconds = intervalDays * 24 * 60 * 60 * 1000;
-
-    const totalIntervals = Math.floor(
-      (endDate.getTime() - startDate.getTime()) / intervalMilliseconds
-    );
-
-    // Generate the intervals with a default value of 0
-    const intervals = Array.from({ length: totalIntervals }, (_, i) => ({
-      key: `${i * intervalDays + 1}-${(i + 1) * intervalDays}`,
-      value: 0,
-    }));
+    const { startDate, endDate, intervals, intervalMilliseconds } =
+      getDateRangeAndIntervals(range);
 
     const orders = await Order.aggregate([
       {
@@ -342,14 +304,7 @@ const getVendorOrders = async (
 
     // console.log(orders);
 
-    // Map the results to the intervals, filling in missing intervals with default value 0
-    orders.forEach(({ _id, count }) => {
-      if (_id === intervals.length)
-        intervals[intervals.length - 1].value += count;
-      if (_id < intervals.length) {
-        intervals[_id].value = count;
-      }
-    });
+    mapDataToIntervals(intervals, orders, 'count');
 
     return intervals;
   } catch (error) {
